3819e28cbbb8b5fc5b345e215b5a272db66bbc73
Roy Saurabh <49860463+roy-saurabh@users.noreply.github.com>
2024-02-08 15:09:42 +0100

    updated intro para

--- .history/main_20240208042046.py
+++ .history/main_20240208042046.py
@@ -14,12 +14,9 @@ st.set_page_config(
 )
 
 # Function to get base64 string
-
-
 def get_image_base64(path):
     with open(path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode()
-
 
 # Convert your image to base64 string
 logo_base64 = get_image_base64("./static/UNESCO_UIS_logo_color_eng.jpg")
@@ -34,7 +31,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.write("# EduDataBot: An AI-enhanced data visualization tool using SDG 4 data")
+st.write("# EduDataBot: Automatic Generation of Visualizations and Infographics using Large Language Models (LLMs)")
 
 st.sidebar.write("## Setup")
 
@@ -57,9 +54,6 @@ else:
 
 st.markdown(
     """
-    This prototype version of the EduDataBot is designed to harness the power of machine learning to better visualize education data. The next development of the tool will include an interactive interface powered by an advanced chatbot. 
-    Fed by the reliable database from the UNESCO Institute for Statistics for SDG 4, it provides a unique and valuable resource for informed decision-making.
-
     EduDataBot leverages python libraries like Llamaindex, Lida, Trulens and AffectLog for generating data visualizations and data-faithful infographics and is grammar agnostic (will work with any programming language and visualization
     libraries e.g. matplotlib, seaborn, altair, d3 etc) and works with multiple large language model providers (OpenAI, Azure OpenAI, PaLM, Cohere, Huggingface). 
     See the project page [here](https://github.com/unesco-uis/un-vision-ai) for updates.
